5bba8fc3bab7094e00cf8d3260d1ff82aa9bc343
lmukembi <lmukembi@gmail.com>
2025-03-17 15:03:42 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -31,18 +31,23 @@ app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("/var/www/kenik/client/build"));
-
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api/admin", require("./routes/admin"));
 app.use("/api/user", require("./routes/user"));
 app.use("/api/reseller", require("./routes/reseller"));
 app.use("/api/payment", require("./routes/payment"));
 
-app.get("*", (req, res) => {
-  res.sendFile("/var/www/kenik/client/build/index.html");
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
+app.get("*", (req, res) => {
+  if (req.originalUrl.startsWith("/api/")) {
+      return res.status(404).json(error.message);
+  }
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 const DBConnection = async () => {
   try {
